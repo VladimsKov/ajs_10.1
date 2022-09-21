355a973e6b3076bb01020c95d142de8ab54f89c5
VladimsKov <vladimskv@yandex.ru>
2022-09-21 22:05:57 +0300

    исправление

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,22 +1,4 @@
-import read from '../reader';
-import json from '../parser';
-import GameSaving from '../gameSavings';
-
-class GameSavingLoader {
-  static load() {
-    return read()
-      .then((data) => json(data))
-      .then((data) => {
-        const res = JSON.parse(data);
-        return new GameSaving(
-          res.id,
-          res.created,
-          res.userInfo,
-        );
-      });
-  }
-}
-
+import GameSavingLoader from '../gameSavingLoader';
 // test resolved
 
 test('check resolve:', () => GameSavingLoader.load()
--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,6 @@
 // TODO: write your code here
 
-import read from './reader';
-import json from './parser';
-import GameSaving from './gameSavings';
-// import GameSavingLoader from './gameSavingLoader';
-
-class GameSavingLoader {
-  static load() {
-    return read()
-      .then((data) => json(data))
-      .then((data) => {
-        const res = JSON.parse(data);
-        return new GameSaving(
-          res.id,
-          res.created,
-          res.userInfo,
-        );
-      });
-  }
-}
+import GameSavingLoader from './gameSavingLoader';
 
 GameSavingLoader.load()
   .then((saving) => saving,
